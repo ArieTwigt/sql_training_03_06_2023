70bbe37b5d4defa9591b096ff1746748f4533e20
Arie Twigt <ariekanarie@twigtd4.nl>
2023-03-22 15:56:42 +0100

    added solutions for advanced

--- assignments/advanced/assignment_a4_2.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    car_type,
-    COUNT(*) as count,
-    (COUNT(*) * 100.0 / (SELECT COUNT(*) FROM registered_cars)) as percentage
-FROM registered_cars
-GROUP BY car_type
-ORDER BY count DESC
-LIMIT 3;
-
--- assignments/advanced/assignment_a5_2.sql
+++
@@ -1,7 +0,0 @@
-/*
-Find the brand and model of the 
-car with the highest net power 
-to weight ratio (sum of 
-net_power_fuel and net_power_electric divided by the price):
-*/
-
